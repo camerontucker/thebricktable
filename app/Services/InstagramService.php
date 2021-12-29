61890ca731af23940e6f5822a9816ef0cdd3d6f6
Cam Tucker <ctucker@regent-college.edu>
2021-12-28 20:58:08 -0600

    instagram!

--- app/Services/InstagramService.php
+++ app/Services/InstagramService.php
@@ -7,6 +7,7 @@ use Dymantic\InstagramFeed\InstagramFeed;
 class InstagramService
 {
     public function getFeed() {
+        return []; // try catch can't handle instantiation without auth
         return InstagramFeed::for(config('services.instagram.feed'));
     }
 }
