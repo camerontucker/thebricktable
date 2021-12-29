37466e72a7b1e4506e065b455c85af3ca8fd3221
Cam Tucker <ctucker@regent-college.edu>
2021-12-28 21:00:57 -0600

    change policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -6,7 +6,7 @@
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $policy !!}
+                No personal data is collected by this website.
             </div>
         </div>
     </div>
